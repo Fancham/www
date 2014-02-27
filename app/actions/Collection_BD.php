f2b314a229ce815f049151ea9e63420d1f1e2ecf
Fancham <fancham@outlook.fr>
2014-02-27 20:58:35 +0100

    Fancham: Pagination OK

--- app/actions/Collection_BD.php
+++ app/actions/Collection_BD.php
@@ -16,6 +16,10 @@ $BL_Recherche=new BL_Recherche();
 
 if (isset($_POST['ListeGenreBD']) or isset($_POST['ListeType']) or isset($_POST['ListeLecteur']))
 {
+	// Un résultat etait deja en session => suppression
+    if(isset($_SESSION['resultat'])) {
+        unset($_SESSION['resultat']);
+    }
 	if (!empty($_POST['ListeGenreBD']))
 	{
 		$genre=$_POST['ListeGenreBD'];
@@ -29,6 +33,21 @@ if (isset($_POST['ListeGenreBD']) or isset($_POST['ListeType']) or isset($_POST[
 		$lecteur=$_POST['ListeLecteur'];
 	}
 	$Resultat=$DAL_Recherche->ListeBDRechercheConditionne($genre,$type,$lecteur);
+	
+	// Récupération du résultat de la requête
+    $Affichage=$Resultat->fetchAll();
+    // Stocke en session la liste récupérée
+    // /!\ Ne pas mettre en session $Resultat car l'objet est non serialisable
+    $_SESSION['resultat']=$Affichage;
+}	else {
+    // Le resultat est il dans la session ?
+    if(isset($_SESSION['resultat'])) {
+        // Oui on le recupere
+        $Affichage=$_SESSION['resultat'];
+    } else {
+        // Non initialise une valeur par defaut
+        $Affichage=array();
+    }
 }
 
 $ReponseBD=$DAL_Recherche->ListeGenreBD();
@@ -36,7 +55,12 @@ $ReponseLecteur=$DAL_Recherche->ListeLecteurBD();
 $ReponseType=$DAL_Recherche->ListeTypeBD();
 
 $pageCourante=getPageCourante('page');
-
-//$contentForLayout = Atomik::render('Collection_BD', Array('Resultat'=>$Resultat, 'pageCourante'=>$pageCourante));
+// $contentForLayout = Atomik::render('Collection_BD', Array('Resultat'=>$Resultat, 
+                                                          // 'pageCourante'=>$pageCourante, 
+                                                          // 'ReponseBD'=>$ReponseBD,
+                                                          // 'ReponseLecteur'=>$ReponseLecteur,
+                                                          // 'ReponseType'=>$ReponseType
+                                                          // )
+                                                          // );
 
 ?>
