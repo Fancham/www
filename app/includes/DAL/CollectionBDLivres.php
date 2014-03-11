137e6d678600a619fc8d71b9e8a565535f4bd6c4
Fancham <fancham@outlook.fr>
2014-03-12 00:08:30 +0100

    Fancham: Correction DAL BD Livres

--- app/includes/DAL/CollectionBDLivres.php
+++ app/includes/DAL/CollectionBDLivres.php
@@ -6,14 +6,14 @@ Atomik::needed('CMN/ConnexionBDD');
 Class DAL_Collection_BDLivres
 {
 	//////////            Recherche de BD             //////////
-	
+
 	// Menu Liste Genre dans la page de sélection des BD
 	function ListeGenreBD()
 	{
 		try
 		{
 			$ConnexionBDD=new CMN_ConnexionBDD();
-			
+
 			$bdd=$ConnexionBDD->Connex();
 			$sql='SELECT DISTINCT genres.Genre FROM Livres INNER JOIN Genres ON Livres.genre=Genres.Id WHERE Type NOT LIKE \'Livre\'';
 			$R=$bdd->query($sql);
@@ -33,7 +33,7 @@ Class DAL_Collection_BDLivres
 		try
 		{
 			$ConnexionBDD=new CMN_ConnexionBDD();
-			
+
 			$bdd=$ConnexionBDD->Connex();
 			$sql='SELECT DISTINCT Type FROM Livres WHERE Type NOT LIKE \'Livre\'';
 			$R=$bdd->query($sql);
@@ -53,7 +53,7 @@ Class DAL_Collection_BDLivres
 		try
 		{
 			$ConnexionBDD=new CMN_ConnexionBDD();
-			
+
 			$bdd=$ConnexionBDD->Connex();
 			$sql='SELECT DISTINCT Lecteurs.Lecteur FROM Livres INNER JOIN Lecteurs ON Livres.Lecteur=Lecteurs.Id WHERE Type NOT LIKE \'Livre\'';
 			$R=$bdd->query($sql);
@@ -73,24 +73,75 @@ Class DAL_Collection_BDLivres
 		try
 		{
 			$ConnexionBDD=new CMN_ConnexionBDD();
-			
 			$bdd=$ConnexionBDD->Connex();
 			if ($genre=='Tout')
 			{
 				$sql='SELECT genres.Genre, Nom, Editeur, Auteur, Titre, Volume, Type, lecteurs.Lecteur FROM Livres INNER JOIN Genres ON Livres.genre=Genres.Id INNER JOIN Lecteurs ON Livres.Lecteur=Lecteurs.Id WHERE Type NOT LIKE \'Livre\' ORDER BY Auteur, Genre, Nom, Titre, Volume';
 				$R=$bdd->prepare($sql);
-				
+
 			}
-			else 
+			else if ($genre!='Vide' && $type!='Vide' && $lecteur!='Vide')
 			{
 				$genre='%'. $genre .'%';
 				$type='%'. $type .'%';
 				$lecteur='%'. $lecteur .'%';
-				$sql='SELECT genres.Genre, Nom, Editeur, Auteur, Titre, Volume, Type, lecteurs.Lecteur FROM Livres INNER JOIN Genres ON Livres.genre=Genres.Id INNER JOIN Lecteurs ON Livres.Lecteur=Lecteurs.Id WHERE Type NOT LIKE \'Livre\' and (lecteurs.lecteur like :lecteur OR type like :type OR genres.genre like :genre) ORDER BY Auteur, Genre, Nom, Titre, Volume';
+				$sql='SELECT genres.Genre, Nom, Editeur, Auteur, Titre, Volume, Type, lecteurs.Lecteur FROM Livres INNER JOIN Genres ON Livres.genre=Genres.Id INNER JOIN Lecteurs ON Livres.Lecteur=Lecteurs.Id WHERE Type NOT LIKE \'Livre\' and (lecteurs.lecteur like :lecteur AND type like :type AND genres.genre like :genre) ORDER BY Auteur, Genre, Nom, Titre, Volume';
 				$R=$bdd->prepare($sql);
 				$R->bindParam(':lecteur', $lecteur, PDO::PARAM_STR);
 				$R->bindParam(':type', $type, PDO::PARAM_STR);
 				$R->bindParam(':genre', $genre, PDO::PARAM_STR);
+			}
+			else if ($genre!='Vide' && $type!='Vide' && $lecteur=='Vide')
+			{
+				$genre='%'. $genre .'%';
+				$type='%'. $type .'%';
+				$sql='SELECT genres.Genre, Nom, Editeur, Auteur, Titre, Volume, Type, lecteurs.Lecteur FROM Livres INNER JOIN Genres ON Livres.genre=Genres.Id INNER JOIN Lecteurs ON Livres.Lecteur=Lecteurs.Id WHERE Type NOT LIKE \'Livre\' and (type like :type AND genres.genre like :genre) ORDER BY Auteur, Genre, Nom, Titre, Volume';
+				$R=$bdd->prepare($sql);
+				$R->bindParam(':type', $type, PDO::PARAM_STR);
+				$R->bindParam(':genre', $genre, PDO::PARAM_STR);
+			}
+			else if ($genre!='Vide' && $type=='Vide' && $lecteur!='Vide')
+			{
+				$genre='%'. $genre .'%';
+				$lecteur='%'. $lecteur .'%';
+				$sql='SELECT genres.Genre, Nom, Editeur, Auteur, Titre, Volume, Type, lecteurs.Lecteur FROM Livres INNER JOIN Genres ON Livres.genre=Genres.Id INNER JOIN Lecteurs ON Livres.Lecteur=Lecteurs.Id WHERE Type NOT LIKE \'Livre\' and (lecteurs.lecteur like :lecteur AND genres.genre like :genre) ORDER BY Auteur, Genre, Nom, Titre, Volume';
+				$R=$bdd->prepare($sql);
+				$R->bindParam(':lecteur', $lecteur, PDO::PARAM_STR);
+				$R->bindParam(':genre', $genre, PDO::PARAM_STR);
+			}
+			else if ($genre=='Vide' && $type!='Vide' && $lecteur!='Vide')
+			{
+				$type='%'. $type .'%';
+				$lecteur='%'. $lecteur .'%';
+				$sql='SELECT genres.Genre, Nom, Editeur, Auteur, Titre, Volume, Type, lecteurs.Lecteur FROM Livres INNER JOIN Genres ON Livres.genre=Genres.Id INNER JOIN Lecteurs ON Livres.Lecteur=Lecteurs.Id WHERE Type NOT LIKE \'Livre\' and (lecteurs.lecteur like :lecteur AND type like :type) ORDER BY Auteur, Genre, Nom, Titre, Volume';
+				$R=$bdd->prepare($sql);
+				$R->bindParam(':lecteur', $lecteur, PDO::PARAM_STR);
+				$R->bindParam(':type', $type, PDO::PARAM_STR);
+			}
+			else if ($genre=='Vide' && $type=='Vide' && $lecteur!='Vide')
+			{
+				$lecteur='%'. $lecteur .'%';
+				$sql='SELECT genres.Genre, Nom, Editeur, Auteur, Titre, Volume, Type, lecteurs.Lecteur FROM Livres INNER JOIN Genres ON Livres.genre=Genres.Id INNER JOIN Lecteurs ON Livres.Lecteur=Lecteurs.Id WHERE Type NOT LIKE \'Livre\' and (lecteurs.lecteur like :lecteur) ORDER BY Auteur, Genre, Nom, Titre, Volume';
+				$R=$bdd->prepare($sql);
+				$R->bindParam(':lecteur', $lecteur, PDO::PARAM_STR);
+			}
+			else if ($genre=='Vide' && $type!='Vide' && $lecteur=='Vide')
+			{
+				$type='%'. $type .'%';
+				$sql='SELECT genres.Genre, Nom, Editeur, Auteur, Titre, Volume, Type, lecteurs.Lecteur FROM Livres INNER JOIN Genres ON Livres.genre=Genres.Id INNER JOIN Lecteurs ON Livres.Lecteur=Lecteurs.Id WHERE Type NOT LIKE \'Livre\' and (type like :type) ORDER BY Auteur, Genre, Nom, Titre, Volume';
+				$R=$bdd->prepare($sql);
+				$R->bindParam(':type', $type, PDO::PARAM_STR);
+			}
+			else if ($genre!='Vide' && $type=='Vide' && $lecteur=='Vide')
+			{
+				$lecteur='%'. $genre .'%';
+				$sql='SELECT genres.Genre, Nom, Editeur, Auteur, Titre, Volume, Type, lecteurs.Lecteur FROM Livres INNER JOIN Genres ON Livres.genre=Genres.Id INNER JOIN Lecteurs ON Livres.Lecteur=Lecteurs.Id WHERE Type NOT LIKE \'Livre\' and (genres.genre like :genre) ORDER BY Auteur, Genre, Nom, Titre, Volume';
+				$R=$bdd->prepare($sql);
+				$R->bindParam(':genre', $genre, PDO::PARAM_STR);
+			}
+			else
+			{
+				$R=array();
 			}
 			$R->execute();
 			return $R;
@@ -101,17 +152,17 @@ Class DAL_Collection_BDLivres
 			die('Erreur:' . $e-> getMessage());
 		}
 	}
-	
-	
-//////////            Recherche de Livres             //////////
-	
+
+
+	//////////            Recherche de Livres             //////////
+
 	// Menu Liste Genre dans la page de sélection des Livres
 	function ListeGenreLivres()
 	{
 		try
 		{
 			$ConnexionBDD=new CMN_ConnexionBDD();
-			
+
 			$bdd=$ConnexionBDD->Connex();
 			$sql='SELECT DISTINCT Genres.Genre FROM Livres INNER JOIN Genres ON Livres.genre=Genres.Id WHERE Type = \'Livre\'';
 			$R=$bdd->query($sql);
@@ -131,7 +182,7 @@ Class DAL_Collection_BDLivres
 		try
 		{
 			$ConnexionBDD=new CMN_ConnexionBDD();
-			
+
 			$bdd=$ConnexionBDD->Connex();
 			$sql='SELECT DISTINCT Auteur FROM Livres WHERE Type = \'Livre\'';
 			$R=$bdd->query($sql);
@@ -151,7 +202,7 @@ Class DAL_Collection_BDLivres
 		try
 		{
 			$ConnexionBDD=new CMN_ConnexionBDD();
-			
+
 			$bdd=$ConnexionBDD->Connex();
 			$sql='SELECT DISTINCT Lecteurs.Lecteur FROM Livres INNER JOIN Lecteurs ON Livres.Lecteur=Lecteurs.Id WHERE Type = \'Livre\'';
 			$R=$bdd->query($sql);
@@ -171,23 +222,75 @@ Class DAL_Collection_BDLivres
 		try
 		{
 			$ConnexionBDD=new CMN_ConnexionBDD();
-			
+
 			$bdd=$ConnexionBDD->Connex();
 			if ($genre=='Tout')
 			{
 				$sql='SELECT genres.Genre, Nom, Editeur, Auteur, Titre, Volume, Type, lecteurs.Lecteur FROM Livres INNER JOIN Genres ON Livres.genre=Genres.Id INNER JOIN Lecteurs ON Livres.Lecteur=Lecteurs.Id WHERE Type = \'Livre\' ORDER BY Auteur, genres.Genre, Titre, Volume';
 				$R=$bdd->prepare($sql);
 			}
-			else 
+			else if ($genre!='Vide' && $auteur!='Vide' && $lecteur!='Vide')
 			{
 				$genre='%'. $genre .'%';
 				$auteur='%'. $auteur .'%';
 				$lecteur='%'. $lecteur .'%';
-				$sql='SELECT genres.Genre, Nom, Editeur, Auteur, Titre, Volume, Type, lecteurs.Lecteur FROM Livres INNER JOIN Genres ON Livres.genre=Genres.Id INNER JOIN Lecteurs ON Livres.Lecteur=Lecteurs.Id WHERE Type = \'Livre\' and (lecteurs.lecteur like :lecteur OR auteur like :auteur OR genres.genre like :genre) ORDER BY Auteur, genres.Genre, Titre, Volume';
+				$sql='SELECT genres.Genre, Nom, Editeur, Auteur, Titre, Volume, Type, lecteurs.Lecteur FROM Livres INNER JOIN Genres ON Livres.genre=Genres.Id INNER JOIN Lecteurs ON Livres.Lecteur=Lecteurs.Id WHERE Type = \'Livre\' and (lecteurs.lecteur like :lecteur AND auteur like :auteur AND genres.genre like :genre) ORDER BY Auteur, genres.Genre, Titre, Volume';
 				$R=$bdd->prepare($sql);
 				$R->bindParam(':lecteur', $lecteur, PDO::PARAM_STR);
 				$R->bindParam(':auteur', $auteur, PDO::PARAM_STR);
 				$R->bindParam(':genre', $genre, PDO::PARAM_STR);
+			}
+			else if ($genre!='Vide' && $auteur!='Vide' && $lecteur=='Vide')
+			{
+				$genre='%'. $genre .'%';
+				$auteur='%'. $auteur .'%';
+				$sql='SELECT genres.Genre, Nom, Editeur, Auteur, Titre, Volume, Type, lecteurs.Lecteur FROM Livres INNER JOIN Genres ON Livres.genre=Genres.Id INNER JOIN Lecteurs ON Livres.Lecteur=Lecteurs.Id WHERE Type = \'Livre\' and (auteur like :auteur AND genres.genre like :genre) ORDER BY Auteur, genres.Genre, Titre, Volume';
+				$R=$bdd->prepare($sql);
+				$R->bindParam(':auteur', $auteur, PDO::PARAM_STR);
+				$R->bindParam(':genre', $genre, PDO::PARAM_STR);
+			}
+			else if ($genre!='Vide' && $auteur=='Vide' && $lecteur!='Vide')
+			{
+				$genre='%'. $genre .'%';
+				$lecteur='%'. $lecteur .'%';
+				$sql='SELECT genres.Genre, Nom, Editeur, Auteur, Titre, Volume, Type, lecteurs.Lecteur FROM Livres INNER JOIN Genres ON Livres.genre=Genres.Id INNER JOIN Lecteurs ON Livres.Lecteur=Lecteurs.Id WHERE Type = \'Livre\' and (lecteurs.lecteur like :lecteur AND genres.genre like :genre) ORDER BY Auteur, genres.Genre, Titre, Volume';
+				$R=$bdd->prepare($sql);
+				$R->bindParam(':lecteur', $lecteur, PDO::PARAM_STR);
+				$R->bindParam(':genre', $genre, PDO::PARAM_STR);
+			}
+			else if ($genre=='Vide' && $auteur!='Vide' && $lecteur!='Vide')
+			{
+				$auteur='%'. $auteur .'%';
+				$lecteur='%'. $lecteur .'%';
+				$sql='SELECT genres.Genre, Nom, Editeur, Auteur, Titre, Volume, Type, lecteurs.Lecteur FROM Livres INNER JOIN Genres ON Livres.genre=Genres.Id INNER JOIN Lecteurs ON Livres.Lecteur=Lecteurs.Id WHERE Type = \'Livre\' and (lecteurs.lecteur like :lecteur AND auteur like :auteur) ORDER BY Auteur, genres.Genre, Titre, Volume';
+				$R=$bdd->prepare($sql);
+				$R->bindParam(':lecteur', $lecteur, PDO::PARAM_STR);
+				$R->bindParam(':auteur', $auteur, PDO::PARAM_STR);
+			}
+			else if ($genre!='Vide' && $auteur=='Vide' && $lecteur=='Vide')
+			{
+				$genre='%'. $genre .'%';
+				$sql='SELECT genres.Genre, Nom, Editeur, Auteur, Titre, Volume, Type, lecteurs.Lecteur FROM Livres INNER JOIN Genres ON Livres.genre=Genres.Id INNER JOIN Lecteurs ON Livres.Lecteur=Lecteurs.Id WHERE Type = \'Livre\' and (genres.genre like :genre) ORDER BY Auteur, genres.Genre, Titre, Volume';
+				$R=$bdd->prepare($sql);
+				$R->bindParam(':genre', $genre, PDO::PARAM_STR);
+			}
+			else if ($genre=='Vide' && $auteur!='Vide' && $lecteur=='Vide')
+			{
+				$auteur='%'. $auteur .'%';
+				$sql='SELECT genres.Genre, Nom, Editeur, Auteur, Titre, Volume, Type, lecteurs.Lecteur FROM Livres INNER JOIN Genres ON Livres.genre=Genres.Id INNER JOIN Lecteurs ON Livres.Lecteur=Lecteurs.Id WHERE Type = \'Livre\' and (auteur like :auteur) ORDER BY Auteur, genres.Genre, Titre, Volume';
+				$R=$bdd->prepare($sql);
+				$R->bindParam(':auteur', $auteur, PDO::PARAM_STR);
+			}
+			else if ($genre=='Vide' && $auteur=='Vide' && $lecteur!='Vide')
+			{
+				$lecteur='%'. $lecteur .'%';
+				$sql='SELECT genres.Genre, Nom, Editeur, Auteur, Titre, Volume, Type, lecteurs.Lecteur FROM Livres INNER JOIN Genres ON Livres.genre=Genres.Id INNER JOIN Lecteurs ON Livres.Lecteur=Lecteurs.Id WHERE Type = \'Livre\' and (lecteurs.lecteur like :lecteur) ORDER BY Auteur, genres.Genre, Titre, Volume';
+				$R=$bdd->prepare($sql);
+				$R->bindParam(':lecteur', $lecteur, PDO::PARAM_STR);
+			}
+			else
+			{
+				$R=array();
 			}
 			$R->execute();
 			return $R;
